62d3b8369bb77b154d7b4e2ae2583ffa09694d00
Shen Chauhan <shenchauhan@hotmail.com>
2016-07-14 20:22:22 -0700

    Adding headers and also ran code formatter over source.

--- UWPTileGenerator/PackageManifestEditor.cs
+++ UWPTileGenerator/PackageManifestEditor.cs
@@ -1,4 +1,15 @@
-﻿using System.IO;
+﻿// ******************************************************************
+// This code is licensed under the MIT License (MIT).
+// THE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
+// ******************************************************************
+
+using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 
@@ -22,7 +33,6 @@ namespace UWPTileGenerator
             var visualElemment = xdocument.Descendants(XName.Get("VisualElements", xmlNamespace)).FirstOrDefault();
             if (visualElemment != null)
             {
-
                 var splashScreen = xdocument.Descendants(XName.Get("SplashScreen", xmlNamespace)).FirstOrDefault();
                 if (splashScreen == null)
                 {
--- UWPTileGenerator/XmlHelper.cs
+++ UWPTileGenerator/XmlHelper.cs
@@ -1,26 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// ******************************************************************
+// This code is licensed under the MIT License (MIT).
+// THE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
+// ******************************************************************
+
 using System.Xml.Linq;
 
 namespace UWPTileGenerator
 {
-	public static class XmlHelper
-	{
-		public static void AddAttribute(this XElement element, string attribute, string value)
-		{
-			var xAttribute = element.Attribute(XName.Get(attribute));
+    public static class XmlHelper
+    {
+        public static void AddAttribute(this XElement element, string attribute, string value)
+        {
+            var xAttribute = element.Attribute(XName.Get(attribute));
 
-			if (xAttribute == null)
-			{
-				element.Add(new XAttribute(XName.Get(attribute), value));
-			}
-			else
-			{
-				element.Attribute(XName.Get(attribute)).Value = value;
-			}
-		}
-	}
+            if (xAttribute == null)
+            {
+                element.Add(new XAttribute(XName.Get(attribute), value));
+            }
+            else
+            {
+                element.Attribute(XName.Get(attribute)).Value = value;
+            }
+        }
+    }
 }
